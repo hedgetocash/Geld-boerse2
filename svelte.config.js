d41c384bab218ae1d98018b8f3e578a57179cc6a
Nils Schüpbach <nilsschupbach@macbook-air-von-nils-9.home>
2025-12-17 00:21:34 +0100

    Fix asset base path via Vite config

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,9 @@
 import adapter from '@sveltejs/adapter-static'
 
-const repoName = 'Geld-boerse2'
-
 export default {
 	kit: {
 		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
 			fallback: 'index.html',
 		}),
-		paths: {
-			base: `/${repoName}`,
-		},
 	},
 }
